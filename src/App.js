237bb10229b42bc8402e8e8108883847a58d5251
RobelsiTejada <robelsitejada@gmail.com>
2017-10-04 22:34:13 -0400

    updating modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import 'react-dom';
 import 'react-modal';
 import './App.css';
@@ -15,7 +14,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fantasy Sports Leagues</h1>
         </header>
         <p className="App-intro">
